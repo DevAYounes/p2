14021966790fc9e737fb360a154a6edcdeb5c793
unknown <DevAYounes@gmail.com>
2023-09-03 13:30:36 +0300

    register change

--- src/app/Register/page.js
+++ src/app/Register/page.js
@@ -1,8 +1,94 @@
-import "./Register.css"
+"use client";
+import "./Register.css";
+import React from "react";
+import { Card, Button, Checkbox, Form, Input } from "antd";
+import { Image } from "antd";
 export default function Home() {
+  const onFinish = (values) => {
+    console.log("Success:", values);
+  };
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
   return (
-    <div className="c1">
-      
+    <div className="c1 ">
+      <div className="register-form">
+        <Card>
+          <Form
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Email"
+              name="Email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Email!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              label="Phone"
+              name="Phone"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Phone!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+      <Image
+        width={200}
+        height={200}
+        style={{ marginTop: "50%", marginLeft: "50%" }}
+        src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+      />
     </div>
-  )
+  );
 }
